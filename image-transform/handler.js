575008c62d5d58174ec82f98c69af240635ce6d0
cjbeattie <courtney.j.beattie+1@gmail.com>
2021-04-13 17:05:57 +0800

    Function now returns base64 encoded image

--- image-transform/handler.js
+++ image-transform/handler.js
@@ -1,4 +1,6 @@
 'use strict';
+// const { type } = require('node:os');
+const sharp = require('sharp');
 
 module.exports.hello = async (event) => {
   return {
@@ -19,18 +21,49 @@ module.exports.hello = async (event) => {
 
 module.exports.imageTransform = async (event) => {
 
+  console.log(event);
+  // const transformer = sharp()
+  //   .negate()
+  //   .toBuffer(function (err, outputBuffer, info) {
+  //     // outputBuffer contains 200px high JPEG image data,
+  //     // auto-rotated using EXIF Orientation tag
+  //     // info.width and info.height contain the dimensions of the resized image
+  //   });
+  // readableStream.pipe(transformer);
+
+  // Extract the image type from the base64 encoded string
+  // const imageTypeEndIndex = event.body.indexOf(';base64,')
+  // const imageType = event.body.slice(5, imageTypeEndIndex)
+  // const headers = JSON.parse(event.headers);
+  // console.log("*** obj is: ", obj);
+  // const imageType = headers.content - type;
+
+  const imageType = event.headers["Content-Type"];
+
+  // Extract the image data from the base64 encoded string
+  const imageData = event.body.split(';base64,').pop() // returns an array, ok?
+  const imgBuffer = Buffer.from(imageData, 'base64');
+
+  const outputBuffer = await sharp(imgBuffer)
+    .negate()
+    .toBuffer()
+    .catch(err => console.log(`error: ${err}`))
+
+  const image = `data:${imageType};base64,${outputBuffer.toString('base64')}`;
+  // .then(data => {
+  //   console.log('success: ', data)
+  //   console.log(`data:image/png;base64,${data.toString('base64')}`)
+  // })
 
 
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: `Here is your file in base64: ${event.body}`,
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      'Content-Type': imageType,
+      // 'Content-Type': 'application/json',
+      // 'Content-Type': 'image/jpeg',
+    },
+    body: image,
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
